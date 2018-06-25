4429acb1542a5333e0defac9778c5cf63925c0f9
Nikita Kazantsev <kaznik2013@gmail.com>
2018-06-25 23:03:44 +0300

    sql support

--- server/main.py
+++ server/main.py
@@ -1,79 +1,102 @@
 from flask import Flask, request, render_template, redirect, make_response
-import hashlib
-import socket
+import sqlite3
+
+conn = sqlite3.connect("test.db")
+
+db = conn.cursor()
+
 app = Flask(__name__)
-
-users = []
-
-user_data = {}
 
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-
     if request.method == "POST":
         login = request.form.get("login", "")
         password = request.form.get("password", "")
         if login == "" or password == "":
-            return "Не были введены оба поля"
+            return "Введите оба поля!!!"
 
-        users.append({"login": login, "password": hashlib.md5(password.encode()).hexdigest()})
+        add_new = "INSERT INTO users (login, password) VALUES ('{}','{}')".format(login, password)
+        db.execute(add_new)
+        conn.commit()
         return redirect("/")
-
     else:
         return render_template("register.html")
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-
     if request.method == "POST":
         login = request.form.get("login", "")
-        password = hashlib.md5((request.form.get("password", "")).encode()).hexdigest()
-
+        password = request.form.get("password", "")
         if login == "" or password == "":
-            return "Не были введены оба поля"
-        for user in users:
-            if user["login"] == login and user["password"] == password:
-                response = make_response(redirect("/"))
-                response.set_cookie("login", login)
-                response.set_cookie("password", password)
-                return response
-        return "Такого пользователя не существует"
-    
-    return render_template('login.html')
+            return "Введите оба поля!!!"
 
+        find_user = "SELECT * FROM users WHERE login = '{}' AND password = '{}'".format(login, password)
 
-@app.route("/", methods=["POST", "GET"])
-def index():
-    login = request.cookies.get("login", "")
-    password=request.cookies.get("password", "")
-    for user in users:
-        if user["login"] == login and user["password"] == password:
-            secret = user_data.get(login, "")
-            return render_template("index.html", name=login, secret=secret)
-    if login=='' and password=='':
-        return redirect("/register")
+        db.execute(find_user)
+
+        user = db.fetchone()
+
+        if user is not None:
+            response = make_response(redirect("/"))
+            response.set_cookie("id", str(user[0]))
+            return response
+        return "Нет такого пользователя"
     else:
-        return redirect("/login")
+        return render_template('login.html')
 
+
+@app.route("/")
+def index():
+    id = request.cookies.get("id", "-1")
+
+    find_secret = "SELECT text FROM secrets WHERE user_id = {}".format(id)
+    db.execute(find_secret)
+    secret = db.fetchone()
+    if secret:
+        secret = secret[0]
+    else:
+        secret = "Empty"
+
+    user_name = "SELECT login FROM users WHERE id = {}".format(id)
+    db.execute(user_name)
+    login = db.fetchone()
+
+    if login:
+        login = login[0]
+    else:
+        login = "Anon"
+
+
+    return render_template("index.html", name=login, secret=secret)
 
 
 @app.route("/add_secret", methods=["POST", "GET"])
 def secret():
-    login = request.cookies.get("login", "")
-    if login == "":
-        return "NO-SYSTEM LOGIN"
+    id = request.cookies.get("id", "")
+    if id == "":
+        return "GO AWAY PLS"
     if request.method == "POST":
         secret = request.form.get("secret", "")
-        user_data[login] = secret
+        insert_secret = "INSERT INTO secrets (text, user_id) VALUES ('{}',{})".format(secret, id)
+        db.execute(insert_secret)
+        conn.commit()
         return redirect("/")
     else:
         return render_template("secret.html")
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', debug=True, port=8000)
-    
+create_users_table = "CREATE TABLE IF NOT EXISTS users " \
+                     "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," \
+                     "login VARCHAR(200), password VARCHAR(200))"
 
+create_secret_table = "CREATE TABLE IF NOT EXISTS secrets " \
+                      "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," \
+                      "text VARCHAR(500), user_id INTEGER)"
+
+db.execute(create_users_table)
+db.execute(create_secret_table)
+conn.commit()
+
+app.run()
